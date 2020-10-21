442fa31efc52355d2d1eebb2b22281ac8f59b07c
VenkataSatyakoppula <60220673+VenkataSatyakoppula@users.noreply.github.com>
2020-10-22 00:01:27 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from backend import *
+from flask import Flask, request, jsonify, render_template, url_for
+
+app = Flask(__name__)
+
+@app.before_first_request
+def start():
+    os.system('''cmd /c "start /min cmd.exe /c backend.exe "''')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.errorhandler(404)
+def error(error):
+    return "404--Not Found"
+
+@app.route('/api')
+def api():
+    word = request.args.get('word')
+    data = autoComplete(list(word.split(" "))[-1])
+    d = dict()
+    for i in range(0,len(data)):
+        d[i] = data[i]
+    return(jsonify(d))
+
+@app.route('/api/save')
+def save():
+    word = request.args.get('data')
+    f = open("static/untitled.txt",'w')
+    f.write(word)
+    f.close()
+    return "0"
+
+if __name__=="__main__":
+    app.run(debug=True)
